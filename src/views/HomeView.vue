689f7ca9bccd7dae979ca5a4e8be6cfbe4feb610
Zeynel <zeynel.kozak@outlook.com>
2022-03-09 08:50:34 +0300

    add svg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="home">
-    <CustomText tag="h1" size="normal" class="cosa">Home</CustomText>
+    <CustomText tag="p" size="normal">Home</CustomText>
+    <CustomText tag="p" size="small">Home</CustomText>
+    <CustomText tag="p" size="xsmall">Home</CustomText>
+    <CustomText tag="p" size="xxsmall">Home</CustomText>
+    <IconHome />
   </div>
 </template>
 
 <script>
 import CustomText from '@/components/CustomText'
+import IconHome from "@/icons/home.svg"
 
 export default {
   name: 'HomeView',
   components: {
-    CustomText
+    CustomText,
+    IconHome
   }
 }
 </script>
 
-<style scoped>
-
-</style>
